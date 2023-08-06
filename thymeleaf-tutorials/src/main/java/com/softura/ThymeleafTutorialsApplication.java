fd45d6db4b7f43478118fedc34ee98525a2b862f
swapnil <sk9137587676@gmail.com>
2023-08-06 14:22:57 +0530

    thymeleaf project..

---
+++ thymeleaf-tutorials/src/main/java/com/softura/ThymeleafTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.softura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTutorialsApplication.class, args);
+	}
+
+}
---
+++ thymeleaf-tutorials/src/test/java/com/softura/ThymeleafTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
